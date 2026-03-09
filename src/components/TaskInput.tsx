c7c9c474dd3bd4751d05fa1a63abe3ebcf8226f8
kobi <kobiokiti@gmail.com>
2026-03-09 14:20:54 +0100

    added error states

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Keyboard } from "react-native";
+import { View, TextInput, Button, StyleSheet, Keyboard, Text } from "react-native";
 
 type TaskInputProps = {
   onAddTask: (title: string) => void;
@@ -7,23 +7,34 @@ type TaskInputProps = {
 
 const TaskInput: React.FC<TaskInputProps> = ({ onAddTask }) => {
   const [title, setTitle] = useState("");
+  const [error, setError] = useState("")
 
   const handleAdd = () => {
-    if (!title.trim()) return; // prevent empty task
+    if (!title.trim()) {
+      setError("Task cannot be empty!");
+      return;
+    }
     onAddTask(title);
-    setTitle(""); // clear input
-    Keyboard.dismiss(); // close keyboard
+    setTitle("");
+    setError("");
+    Keyboard.dismiss();
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter a new task"
-        value={title}
-        onChangeText={setTitle}
-        onSubmitEditing={handleAdd} // allows pressing Enter/Return to add
-      />
+      <View style={{ flex: 1 }}>
+        <TextInput
+          style={[styles.input, error ? styles.inputError : null]}
+          placeholder="Enter a new task"
+          value={title}
+          onChangeText={text => {
+            setTitle(text);
+            if (error) setError("");
+          }}
+          onSubmitEditing={handleAdd}
+        />
+        {error ? <Text style={styles.errorText}>{error}</Text> : null}
+      </View>
       <Button title="Add" onPress={handleAdd} />
     </View>
   );
@@ -31,18 +42,27 @@ const TaskInput: React.FC<TaskInputProps> = ({ onAddTask }) => {
 
 const styles = StyleSheet.create({
   container: {
+    height: 50,
     flexDirection: "row",
     paddingHorizontal: 16,
     marginVertical: 12,
   },
   input: {
-    flex: 1,
+    // flex: 1,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 8,
     paddingHorizontal: 12,
     marginRight: 8,
     height: 40,
+  },
+  inputError: {
+    borderColor: "#FF5252",
+  },
+  errorText: {
+    color: "#FF5252",
+    fontSize: 12,
+    marginTop: 4,
   },
 });
 
